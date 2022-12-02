62890e82a53ece46315f900b9afe672e0f082210
Alain Gourvès <alain.gourves@gmail.com>
2022-12-02 09:36:20 +0100

    uses Map

--- src/js/gradient.js
+++ src/js/gradient.js
@@ -1,31 +1,17 @@
-const colors = [
-    'hsl(233deg 22% 7%)',
-    'hsl(224deg 30% 12%)',
-    'hsl(218deg 41% 17%)',
-    'hsl(213deg 54% 21%)',
-    'hsl(210deg 59% 26%)',
-    'hsl(212deg 47% 34%)',
-    'hsl(213deg 40% 42%)',
-    'hsl(213deg 35% 50%)',
-    'hsl(210deg 42% 58%)',
-    'hsl(205deg 48% 67%)',
-    'hsl(201deg 57% 76%)',
-    'hsl(197deg 77% 86%)'
-];
-const stops = [
-    0,
-    0.28,
-    0.36,
-    0.42,
-    0.46,
-    0.49,
-    0.52,
-    0.56,
-    0.6,
-    0.65,
-    0.74,
-    1
-];
+const colorsMap = new Map([
+    [0,     'hsl(233deg 22% 7%)'],
+    [0.28,  'hsl(224deg 30% 12%)'],
+    [0.36,  'hsl(218deg 41% 17%)'],
+    [0.42,  'hsl(213deg 54% 21%)'],
+    [0.46,  'hsl(210deg 59% 26%)'],
+    [0.49,  'hsl(212deg 47% 34%)'],
+    [0.52,  'hsl(213deg 40% 42%)'],
+    [0.56,  'hsl(213deg 35% 50%)'],
+    [0.6,   'hsl(210deg 42% 58%)'],
+    [0.65,  'hsl(205deg 48% 67%)'],
+    [0.74,  'hsl(201deg 57% 76%)'],
+    [1,     'hsl(197deg 77% 86%)']
+]);
 class Gradient {
     constructor() {
         this.canvasWidth = 5;
@@ -37,9 +23,9 @@ class Gradient {
         this.ctx = this.canvas.getContext('2d', { willReadFrequently: true, alpha: false });
 
         this.gradient = this.ctx.createLinearGradient(0, 0, 0, 100);
-        colors.forEach((col, idx) => {
-            this.gradient.addColorStop(stops[idx], col);
-        })
+        colorsMap.forEach((color, stop) => {
+            this.gradient.addColorStop(stop, color);
+        });
         this.ctx.fillStyle = this.gradient;
         this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
     }
@@ -68,10 +54,11 @@ class Gradient {
 
     // returns the gradient in CSS form
     get CSSLinearGradient() {
-        const val = colors.reduce((accumulator, col, idx) => {
-            return `${accumulator}, ${col} ${Math.round(stops[idx]*100)}%`;
+        let values = '';
+        colorsMap.forEach((color, stop) => {
+            values += `${color} ${Math.round(stop * 100)}%, `;
         });
-        return `linear-gradient(-90deg, ${val})`;
+        return `linear-gradient(-90deg, ${values.slice(0, -2)})`;
     }
 }
 
